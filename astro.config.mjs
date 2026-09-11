345ac18d657ec365c3d3bdb9eefbf51883dbab8b
jctneko <johnson780510@gmail.com>
2026-09-11 16:25:44 +0800

    fix: configure site and base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://johnsonchung.github.io',
+  base: '/taipei-anime-walk',
   output: 'static',
   integrations: [tailwind({ applyBaseStyles: false })]
 });
